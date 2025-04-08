452da4bf7a60f32ab1c624c67f357c9de2975341
aribamuskan <aribamuskan40@gmail.com>
2025-04-09 02:43:16 +0500

    Message

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>hello frontend</h1>
+    </div>
+  );
+}
+
+export default App;
